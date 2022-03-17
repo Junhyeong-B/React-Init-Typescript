4715374488d1380d0b139801aff9f7b52bafa77c
JunHyeongB <aowlr0513@gmail.com>
2022-03-17 16:30:27 +0900

    chore: baseUrl 및 webpack alias 설정 추가

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,9 @@ module.exports = {
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
+    alias: {
+      "@src": path.resolve(__dirname, "src"),
+    },
   },
   devServer: {
     port: 3000,
